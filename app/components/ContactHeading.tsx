2d3f0d12be2a89de2e0eb25cd9f8c9994463ab89
ibrarli <ibraralihaidar@gmail.com>
2025-05-27 05:26:06 -0700

    Fixed Contact Us page content and linked them

--- app/components/ContactHeading.tsx
+++ app/components/ContactHeading.tsx
@@ -1,29 +1,44 @@
 import { Mail, Phone, MapPin } from "lucide-react";
+import React from "react";
+import Link from "next/link";
+import { link } from "fs";
 
 const ContactHeading = () => {
   const contactLinks = [
     {
       icon: <Phone className="w-5 h-5" />,
-      label: "Phone",
-      value: "+92 300 1234567",
+      label: "CEO / Founder",
+      value: "+92 343 8028866",
+      link: "tel:+923438028866", 
+    },
+    {
+      icon: <Phone className="w-5 h-5" />,
+      label: "Business Development Manager",
+      value: "+92 304 8332219",
+      link: "tel:+923048332219",
     },
     {
       icon: <Mail className="w-5 h-5" />,
       label: "Email",
-      value: "info@bluechiplabs.pk",
+      value: "info@carlteck.com",
+      link: "mailto:info@carlteck.com",
     },
     {
       icon: <MapPin className="w-5 h-5" />,
       label: "Location",
       value: "Bluechip Labs, BUITEMS Takatu Campus, Airport Road, Quetta, Pk.",
+      link: "https://maps.app.goo.gl/XRw62puVi2mGFkSo9",
     },
   ];
 
   return (
     <div className=" mb-10 text-center lg:text-left">
-      <h2 className="text-3xl md:text-4xl font-bold mb-2">We’d love to hear from you</h2>
+      <h2 className="text-3xl md:text-4xl font-bold mb-2">
+        We’d love to hear from you
+      </h2>
       <p className="text-gray-600 dark:text-gray-300 mb-6">
-        Whether you have a question, a business inquiry, or want to collaborate — we’re here.
+        Whether you have a question, a business inquiry, or want to collaborate
+        — we’re here.
       </p>
 
       <div className="flex flex-col justify-center  gap-6">
@@ -32,10 +47,16 @@ const ContactHeading = () => {
             key={item.label}
             className="flex items-center gap-3 p-3 rounded-md transition-colors hover:border-solid hover:border-1 hover:border-cyan-600 dark:hover:border-solid cursor-pointer"
           >
-            <span className="text-cyan-600 dark:text-cyan-400">{item.icon}</span>
+            <span className="text-cyan-600 dark:text-cyan-400">
+              {item.icon}
+            </span>
             <div className="text-left">
-              <p className="font-semibold text-black dark:text-white">{item.label}</p>
-              <p className="text-sm text-gray-600 dark:text-gray-300">{item.value}</p>
+              <p className="font-semibold text-black dark:text-white">
+                  <Link href={item.link}>{item.label}</Link>
+              </p>
+              <p className="text-sm text-gray-600 dark:text-gray-300">
+                {item.value}
+              </p>
             </div>
           </div>
         ))}
