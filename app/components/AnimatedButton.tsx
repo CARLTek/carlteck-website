9104f9f2f1954555ceb465780666fb8667739192
ibrarli <ibraralihaidar@gmail.com>
2025-06-04 05:36:57 -0700

    fixed bug

--- app/components/AnimatedButton.tsx
+++ app/components/AnimatedButton.tsx
@@ -13,14 +13,14 @@ const AnimatedButton = ({ label, href }: AnimatedButtonProps) => {
   return (
     <div>
       <Link href={href}>
-        <div className="relative group">
-          <div className="relative w-64 h-14 opacity-90 overflow-hidden rounded-xl bg-black z-10">
+        <div className="relative group ">
+          <div className="relative w-64 h-14 opacity-90 overflow-hidden rounded-xl bg-black z-10  cursor-crosshair ">
             <div className="absolute z-10 -translate-x-44 group-hover:translate-x-[30rem] ease-in transistion-all duration-700 h-full w-44 bg-gradient-to-r from-gray-500 to-white/10 opacity-30 -skew-x-12"></div>
 
             <div className="absolute flex items-center justify-center text-white z-[1] opacity-90 rounded-2xl inset-0.5 bg-black">
               <button
                 name="text"
-                className="input font-semibold text-lg h-full opacity-90 w-full px-16 py-3 rounded-xl text-nowrap bg-black"
+                className="input font-semibold text-lg cursor-crosshair h-full opacity-90 w-full px-16 py-3 rounded-xl text-nowrap  bg-black"
               >
                 {label}
               </button>
